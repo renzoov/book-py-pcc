544253d411dd00674754aba8dbf85450923186ca
Renzo Osorio <renzo.osorio1909@gmail.com>
2023-09-24 23:31:23 -0500

    añadir capitulo 3

---
+++ chapter-03/cars.py
@@ -0,0 +1,32 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+cars.sort()
+print(cars)
+
+cars.sort(reverse=True)
+print(cars)
+
+print("\n------------------------------\n")
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
+
+print("\n------------------------------\n")
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+cars.reverse()
+print(cars)
+
+print("\n------------------------------\n")
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars))
---
+++ chapter-03/motorcycles.py
@@ -0,0 +1,65 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+print("\n------------------------------\n")
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles.append('kawazaki')
+print(motorcycles)
+
+print("\n------------------------------\n")
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles.insert(0, 'pulsar')
+print(motorcycles)
+motorcycles.insert(3, 'ktm')
+print(motorcycles)
+
+print("\n------------------------------\n")
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+del motorcycles[2]
+print(motorcycles)
+
+print("\n------------------------------\n")
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+popped_motorcycle = motorcycles.pop()
+first_owned = motorcycles.pop(0)
+print(motorcycles)
+print(popped_motorcycle)
+print(f"The first motorcycle I owned was a {first_owned.title()}.")
+print(f"The last motorcycle I owned was a {popped_motorcycle.title()}.")
+
+print("\n------------------------------\n")
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+motorcycles.remove('honda')
+too_expensive = "ducati"
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
+
+print("\n------------------------------\n")
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+print(motorcycles[-1])
+
+print("\n------------------------------\n")
+
+motorcycles = []
+print(motorcycles[-1])
