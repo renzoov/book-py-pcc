816c721c4449d2f734e7ae9c088011754de775a9
Renzo Osorio Valdez <renzo.osorio1909@gmail.com>
2026-03-15 21:56:43 -0500

    agregar clases

---
+++ ch09/car.py
@@ -0,0 +1,36 @@
+﻿class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        log_name =  f'{self.year} {self.make} {self.model}'
+        return log_name.title()
+
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+class Battery:
+    def __init__(self, battery_size=40):
+        self.battery_size = battery_size
+    def describe_battery(self):
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 65:
+            range = 225
+
+        print(f"This car can go about {range} miles on a full charge.")
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+my_new_car = Car('audi', 'a4', 2019)
+print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
