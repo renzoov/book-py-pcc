a73e9af194a2c9d99317a3960e61ac770b87955b
Renzo Osorio Valdez <renzo.osorio1909@gmail.com>
2026-03-27 20:20:41 -0500

    agregar capitulos 15 y 16

---
+++ ch15/mpl_squares.py
@@ -0,0 +1,19 @@
+﻿import matplotlib.pyplot as plt
+
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+
+plt.style.use('seaborn-v0_8-dark')
+
+fig, ax = plt.subplots()
+ax.plot(input_values, squares, linewidth=3)
+
+# Establecer el título del gráfico y las etiquetas de los ejes
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Establecer el tamaño de las etiquetas de las marcas
+ax.tick_params(labelsize=14)
+
+plt.show()
